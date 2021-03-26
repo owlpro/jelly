f7aeb8a557bfab9659e46c1d0a7ec41e1a02fff8
Mahdi Amiri <mdpro.smm@gmail.com>
2021-03-26 17:35:36 +0430

    update export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import * as foo from './module.bundle'
 import { SmartCrudProvider } from './module'
 
-console.log(foo)
 ReactDOM.render(
     // <React.StrictMode>
     <SmartCrudProvider>
