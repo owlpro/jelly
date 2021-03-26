b4859eb6a21196079230081a048423748a965a0d
Mahdi Amiri <mdpro.smm@gmail.com>
2021-03-26 21:54:08 +0430

    try to debug 102

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { SmartCrudProvider } from './module'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App'
+// import { SmartCrudProvider } from './module'
+// console.log(module)
+// ReactDOM.render(
+//     // <React.StrictMode>
+//     <SmartCrudProvider>
+//         <App />
+//     </SmartCrudProvider>,
+//     // </React.StrictMode>,
+//     document.getElementById('root')
+// )
 
-ReactDOM.render(
-    // <React.StrictMode>
-    <SmartCrudProvider>
-        <App />
-    </SmartCrudProvider>,
-    // </React.StrictMode>,
-    document.getElementById('root')
-)
+const SmartCrudProvider = require('./package/SmartCrudProvider.js').default
+const SmartCrud = require('./package/SmartCrud.js').default
+
+module.exports = {
+    SmartCrudProvider,
+    SmartCrud,
+}
