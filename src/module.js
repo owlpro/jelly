68242631358f58205706fc80b82ec04f775476fa
Mahdi Amiri <mdpro.smm@gmail.com>
2021-03-26 16:51:22 +0430

    update

--- src/module.js
+++ src/module.js
@@ -1,4 +1,4 @@
 import SmartCrud from './package/SmartCrud'
 import SmartCrudProvider from './package/SmartCrudProvider'
 
-export { SmartCrudProvider, SmartCrud }
+module.exports = { SmartCrudProvider, SmartCrud }
