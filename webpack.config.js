e422792409e841517d154d6e72514016835b40be
Mahdi Amiri <mdpro.smm@gmail.com>
2021-03-27 19:44:37 +0430

    try to debug webpack 110

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,10 @@ module.exports = {
         path: path.join(__dirname, '/dist'),
         filename: 'module.bundle.js',
         // libraryTarget: 'commonjs2',
-        library: "jelly-smart-crud",      
+        library: 'jelly-smart-crud',
         libraryTarget: 'umd',
         publicPath: '/dist/',
-        umdNamedDefine: true 
+        umdNamedDefine: true,
     },
     module: {
         rules: [
@@ -32,5 +32,20 @@ module.exports = {
                 },
             },
         ],
+    },
+    externals: {
+        // Don't bundle react or react-dom
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'React',
+            root: 'React',
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'ReactDOM',
+            root: 'ReactDOM',
+        },
     },
 }
