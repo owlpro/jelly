42ef7c45a1cac3428113d9cb97f9945004db73ef
Mahdi Amiri <mdpro.smm@gmail.com>
2021-03-27 22:25:14 +0430

    try to debug 123

--- src/package/SmartCrudProvider.js
+++ src/package/SmartCrudProvider.js
@@ -1,20 +1,15 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { Provider } from 'react-redux'
 import store from './Redux/store'
 import Media from './Media'
-class SmartCrudProvider extends Component {
-    renderMediaModel = () => {
-        return <Media />
-    }
-    render() {
-        return (
-            <Fragment>
-                <Provider store={store}>
-                    {this.renderMediaModel()}
-                    {this.props.children}
-                </Provider>
-            </Fragment>
-        )
-    }
+
+export default function (props) {
+    return (
+        <Fragment>
+            <Provider store={store}>
+                <Media />
+                {props.children}
+            </Provider>
+        </Fragment>
+    )
 }
-export default SmartCrudProvider
