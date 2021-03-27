77c2ff34a1262afe1c564d0824afc3b56d5939b1
Mahdi Amiri <mdpro.smm@gmail.com>
2021-03-28 00:36:29 +0430

    try to debug 131

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { SmartCrudProvider } from './module'
+
 ReactDOM.render(
     // <React.StrictMode>
     <SmartCrudProvider>
--- webpack.config.js
+++ webpack.config.js
@@ -43,27 +43,9 @@ module.exports = {
             react: path.resolve('./node_modules/react'),
             'react-dom': path.resolve('./node_modules/react-dom'),
             'react-redux': path.resolve('./node_modules/react-redux'),
-            '@material-ui': path.resolve('./node_modules/@material-ui'),
         },
     },
     target: 'node',
     externals: [nodeExternals()],
     plugins: [new webpack.HotModuleReplacementPlugin()],
-    // resolve: {
-    //     extensions: ['.js', '.jsx'],
-    // },
-    // externals: [
-    //     {
-    //         'escape-string-regexp': 'escape-string-regexp',
-    //         flat: 'flat',
-    //         linkifyjs: 'linkifyjs',
-    //         'snake-case': 'snake-case',
-    //         react: {
-    //             root: 'React',
-    //             commonjs2: 'react',
-    //             commonjs: 'react',
-    //             amd: 'react',
-    //         },
-    //     },
-    // ],
 }
