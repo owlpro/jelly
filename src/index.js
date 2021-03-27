2dd1b4f8b6080a1d907f5c33e45a223045595f35
Mahdi Amiri <mdpro.smm@gmail.com>
2021-03-27 19:29:52 +0430

    try to debug 107

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import App from './App'
-// import { SmartCrudProvider } from './module'
-// console.log(module)
-// ReactDOM.render(
-//     // <React.StrictMode>
-//     <SmartCrudProvider>
-//         <App />
-//     </SmartCrudProvider>,
-//     // </React.StrictMode>,
-//     document.getElementById('root')
-// )
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { SmartCrudProvider } from './module'
+ReactDOM.render(
+    // <React.StrictMode>
+    <SmartCrudProvider>
+        <App />
+    </SmartCrudProvider>,
+    // </React.StrictMode>,
+    document.getElementById('root')
+)
 
-const SmartCrudProvider = require('./package/SmartCrudProvider.js').default
-const SmartCrud = require('./package/SmartCrud.js').default
+// const SmartCrudProvider = require('./package/SmartCrudProvider.js').default
+// const SmartCrud = require('./package/SmartCrud.js').default
 
-module.exports = {
-    SmartCrudProvider,
-    SmartCrud,
-}
+// module.exports = {
+//     SmartCrudProvider,
+//     SmartCrud,
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path')
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/module.js',
     mode: 'production',
     output: {
         path: path.join(__dirname, '/dist'),
-        filename: 'index.js',
-        libraryTarget: 'commonjs2',
+        filename: 'module.bundle.js',
+        // libraryTarget: 'commonjs2',
+        library: "jelly-smart-crud",      
+        libraryTarget: 'umd',
+        publicPath: '/dist/',
+        umdNamedDefine: true 
     },
     module: {
         rules: [
@@ -16,17 +20,6 @@ module.exports = {
                     loader: 'babel-loader',
                 },
             },
-            // {
-            //     test: /\.js$/,
-            //     include: path.resolve(__dirname, 'src'),
-            //     exclude: /(node_modules|bower_components|build)/,
-            //     use: {
-            //         loader: 'babel-loader',
-            //         options: {
-            //             presets: ['env'],
-            //         },
-            //     },
-            // },
             {
                 test: /\.css$|\.scss$/,
                 use: ['style-loader', 'css-loader'],
