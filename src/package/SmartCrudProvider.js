86b0d6cb76dcf23050d78e4a8f00de01b188e563
Mahdi Amiri <mdpro.smm@gmail.com>
2021-03-28 17:22:55 +0430

    try to debug 4

--- src/package/SmartCrudProvider.js
+++ src/package/SmartCrudProvider.js
@@ -1,11 +1,15 @@
+import { Button } from '@material-ui/core'
 import React, { Fragment } from 'react'
 import './Assets/scss/styles.scss'
-import Media from './Media/index'
+// import Media from './Media/index'
 
 export default function (props) {
     return (
         <Fragment>
-            <Media />
+            {/* <Media /> */}
+            <h1>this is testing plugin for developing smart crud</h1>
+            <Button variant="contained">foooo</Button>
+            
             {props.children}
         </Fragment>
     )
