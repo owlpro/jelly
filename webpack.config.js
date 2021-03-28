58aa33a6bad291008677dab3020a670c43f02a73
Mahdi Amiri <mdpro.smm@gmail.com>
2021-03-28 18:45:44 +0430

    try to debug 13

--- webpack.config.js
+++ webpack.config.js
@@ -39,17 +39,13 @@ module.exports = {
             },
         ],
     },
-    // resolve: {
-    //     alias: {
-    //         react: path.resolve('./node_modules/react'),
-    //         'react-dom': path.resolve('./node_modules/react-dom'),
-    //         'react-redux': path.resolve('./node_modules/react-redux'),
-    //         '@material-ui/core': path.resolve('./node_modules/@material-ui/core'),
-    //         '@material-ui/icons': path.resolve('./node_modules/@material-ui/icons'),
-    //         '@material-ui/lab': path.resolve('./node_modules/@material-ui/lab'),
-    //         '@material-ui/pickers': path.resolve('./node_modules/@material-ui/pickers'),
-    //     },
-    // },
+    resolve: {
+        alias: {
+            react: path.resolve('./node_modules/react'),
+            'react-dom': path.resolve('./node_modules/react-dom'),
+            'react-redux': path.resolve('./node_modules/react-redux'),
+        },
+    },
     target: 'node',
     externals: [nodeExternals()],
     plugins: [new webpack.HotModuleReplacementPlugin(), new WebpackCleanupPlugin()],
